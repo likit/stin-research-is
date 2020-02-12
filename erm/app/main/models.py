b19d06bc27c06331552ee447f22f537314d93fc8
Likit Preeyanon <likit.pre@mahidol.edu>
2020-02-12 21:44:24 +0700

    added auth blueprint for authentication
    added User model to the main blueprint
    
    updated the wsgi file

---
+++ erm/app/main/models.py
@@ -0,0 +1,23 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column('email', db.String(255), unique=True, nullable=False)
+    password_hash = db.Column('password_hash', db.String(32), nullable=False)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def password(self):
+        raise ValueError('Password attribute is not accessible.')
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
--- erm/wsgi.py
+++ erm/wsgi.py
@@ -1,4 +1,10 @@
 from app import create_app
+from flask_admin.contrib.sqla import ModelView
+from app import admin, db
+from app.main.models import User
+
 
 app = create_app()
+
+admin.add_view(ModelView(User, db.session))
 
