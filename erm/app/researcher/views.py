4e09e1df314486138eabcea279f414d6f8d293a5
Likit Preeyanon <likit.pre@mahidol.edu>
2020-02-13 04:52:34 +0700

    added a view + page for a profile detail
    updated the navbar

--- erm/app/researcher/views.py
+++ erm/app/researcher/views.py
@@ -1,9 +1,17 @@
 from flask import render_template
 from flask_login import login_required
+from app.main.models import User
+from app.researcher.models import Profile
+from app import db
 from . import researcher_bp as researcher
 
 
-@researcher.route('/profile')
+@researcher.route('/profile/<int:user_id>')
 @login_required
-def show_profile():
-    return render_template('researcher/profile.html')
+def show_profile(user_id):
+    user = User.query.get(user_id)
+    if not user.profile:
+        profile = Profile(user_id=user.id)
+        db.session.add(profile)
+        db.session.commit()
+    return render_template('researcher/profile.html', user=user)
