8103b226f400c55c4f91b75f3491ce5851b1d6c0
Likit Preeyanon <likit.pre@mahidol.edu>
2020-02-28 05:17:04 +0700

    refactored the project list view and page
    updated the nav bar accordingly

--- erm/app/project/views.py
+++ erm/app/project/views.py
@@ -3,14 +3,20 @@ from flask import render_template
 from app import db
 from . import project_bp as project
 from .models import *
+from app.main.models import User
 
 
 @project.route('/')
-@project.route('/<int:user_id>')
-def list_projects(user_id=None):
-    projects = ProjectRecord.query.order_by(ProjectRecord.created_at)
+def list_projects():
+    projects = ProjectRecord.query.all()
     return render_template('project/index.html', projects=projects)
 
+
+@project.route('/user/<int:user_id>')
+def list_created_projects(user_id):
+    user = User.query.get(user_id)
+    projects = user.projects
+    return render_template('project/created_projects.html', projects=projects)
 
 @project.route('/detail/<int:project_id>')
 def display_project(project_id):
