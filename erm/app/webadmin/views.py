a32ab821447239d2d7eb66bc46d21359480feff1
Likit Preeyanon <likit.pre@mahidol.edu>
2020-03-13 11:14:53 +0700

    added navbar link to the submissions
    added a view and template for submissions

--- erm/app/webadmin/views.py
+++ erm/app/webadmin/views.py
@@ -1,6 +1,9 @@
 from app.webadmin import webadmin_bp as webadmin
+from flask import render_template, redirect, url_for
+from app.project.models import ProjectRecord
 
 
 @webadmin.route('/submissions')
 def list_submissions():
-    return 'here you go.'
+    submissions = ProjectRecord.query.filter_by(status='submitted')
+    return render_template('webadmin/submissions.html', submissions=submissions)
