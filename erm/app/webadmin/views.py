b784b760494cbfd62d646d9fa2ed3bb3960955ce
Likit Preeyanon <likit.pre@mahidol.edu>
2020-03-18 05:54:46 +0700

    added views and updated pages for reviewers' CRUD

--- erm/app/webadmin/views.py
+++ erm/app/webadmin/views.py
@@ -1,6 +1,8 @@
 from app.webadmin import webadmin_bp as webadmin
-from flask import render_template, redirect, url_for
-from app.project.models import ProjectRecord, ProjectReviewerGroup
+from wsgi import db
+from flask import render_template, redirect, url_for, request, flash
+from app.project.models import (ProjectRecord, ProjectReviewerGroup,
+                                ProjectReviewer, ProjectReviewRecord)
 
 
 @webadmin.route('/submissions')
@@ -16,9 +18,59 @@ def submission_detail(project_id):
 
 
 @webadmin.route('/submissions/<int:project_id>/reviewers/add')
-def add_reviewers(project_id):
+def list_reviewers(project_id):
     project = ProjectRecord.query.get(project_id)
     groups = ProjectReviewerGroup.query.all()
     return render_template('webadmin/reviewers_add.html',
                            project=project,
                            groups=groups)
+
+
+@webadmin.route('/submissions/<int:project_id>/reviewers/add/<int:reviewer_id>')
+def add_reviewer(project_id, reviewer_id):
+    review = ProjectReviewRecord.query.filter_by(reviewer_id=reviewer_id,
+                                                 project_id=project_id).first()
+    if not review:
+        review = ProjectReviewRecord(
+            project_id=project_id,
+            reviewer_id=reviewer_id
+        )
+        db.session.add(review)
+        db.session.commit()
+        flash('Reviewer has been added to the project review.', 'success')
+    return redirect(request.referrer)
+
+
+@webadmin.route('/submissions/<int:project_id>/reviewers/add/all/<int:group_id>')
+def add_all_reviewers(project_id, group_id):
+    reviewer_group = ProjectReviewerGroup.query.get(group_id)
+    project = ProjectRecord.query.get(project_id)
+    for reviewer in reviewer_group.reviewers:
+        if reviewer not in project.reviewers:
+            review = ProjectReviewRecord(reviewer=reviewer, project=project)
+            db.session.add(review)
+    db.session.commit()
+    flash('All reviewers have been added to the project review.', 'success')
+    return redirect(request.referrer)
+
+
+
+@webadmin.route('/submissions/<int:project_id>/reviewers/remove/<int:reviewer_id>')
+def remove_reviewer(project_id, reviewer_id):
+    review = ProjectReviewRecord.query.filter_by(reviewer_id=reviewer_id,
+                                                 project_id=project_id).first()
+    db.session.delete(review)
+    db.session.commit()
+    flash('Reviewer has been removed to the project review.', 'success')
+    return redirect(request.referrer)
+
+
+@webadmin.route('/submissions/<int:project_id>/reviewers/remove/all')
+def remove_all_reviewers(project_id):
+    project = ProjectRecord.query.get(project_id)
+    for review in project.reviews:
+        db.session.delete(review)
+        db.session.commit()
+    flash('All reviewers have been removed to the project review.', 'success')
+    return redirect(request.referrer)
+
