2f17ce59a909762849d5e02d39e0112ea7d9ce72
Likit Preeyanon <likit.pre@mahidol.edu>
2020-03-18 15:08:39 +0700

    added ethical review forms

--- erm/app/webadmin/forms.py
+++ erm/app/webadmin/forms.py
@@ -1,10 +1,14 @@
-from app.project.models import ProjectReviewSendRecord, ProjectReviewRecord
+from app.project.models import (ProjectReviewSendRecord,
+                                ProjectReviewRecord,
+                                ProjectEthicReviewRecord,
+                                ProjectEthicReviewSendRecord)
 from wsgi import db
 from wtforms_alchemy import (model_form_factory, QuerySelectField)
 from flask_wtf import FlaskForm
 
 
 BaseModelForm = model_form_factory(FlaskForm)
+
 
 class ModelForm(BaseModelForm):
     @classmethod
@@ -20,3 +24,13 @@ class ProjectReviewSendRecordForm(BaseModelForm):
 class ProjectReviewRecordForm(BaseModelForm):
     class Meta:
         model = ProjectReviewRecord
+
+
+class ProjectEthicReviewSendRecordForm(BaseModelForm):
+    class Meta:
+        model = ProjectEthicReviewSendRecord
+
+
+class ProjectEthicReviewRecordForm(BaseModelForm):
+    class Meta:
+        model = ProjectEthicReviewRecord
