795a060091f16c90451827abf7f6a6917a39c2ca
Likit Preeyanon <likit.pre@mahidol.edu>
2020-09-28 22:07:34 +0700

    added final report url field

--- erm/app/project/models.py
+++ erm/app/project/models.py
@@ -110,6 +110,7 @@ class ProjectRecord(db.Model):
     creator = db.relationship('User', backref=db.backref('projects'), info={'label': 'ผู้บันทึกข้อมูล'})
     contract_no = db.Column('contract_no', db.String(), nullable=True, info={'label': 'หมายเลขสัญญา'})
     contract_url = db.Column('contract_url', db.String(), nullable=True)
+    final_report_url = db.Column('final_report_url', db.String(), nullable=True, info={'label': 'รายงานฉบับสมบูรณ์'})
     mentor = db.Column('mentor', db.String(), nullable=True, info={'label': 'ที่ปรึกษา'})
 
     @property
